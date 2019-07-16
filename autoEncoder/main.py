49f3ae7c729299cb6a9c42786ece66ac6be9c95d
Sara HAMIDA <52857308+S4r4hFUN@users.noreply.github.com>
2019-07-16 17:33:28 +0100

    Create main.py
    Transpose and stability fixes

---
+++ autoEncoder/main.py
@@ -0,0 +1,33 @@
+from autoencoder.Experiment import Experiment
+from autoencoder.Autoencoder import Autoencoder
+
+import numpy as np
+
+autoencoder_parameters_range = {'hidden1_units': [700],
+                                'regularisation': [0.2],
+                                'learning_rate0': [0.001],
+                                'learning_decay': [0.9],
+                                'batch_size_evaluate': [100],
+                                'batch_size_train': [35],
+                                'nb_epoch': [5],
+                                'is_test': [0]
+                                }
+
+sets_parameters = {'database_id': [0],
+                   'test_ratio': [0.1],
+                   'validation_ratio': [0.1],
+                   'learning_type': 'V'
+                   }
+
+experiment_parameters = {'mean_iterations': [1],
+                         'nb_draws': [1]
+                         }
+
+parameters_range = {'autoencoder': autoencoder_parameters_range,
+                    'experiments': experiment_parameters,
+                    'sets': sets_parameters
+                    }
+
+Experiment = Experiment(parameters_range=parameters_range,
+                        Autoencoder=Autoencoder)
+Experiment.run()
