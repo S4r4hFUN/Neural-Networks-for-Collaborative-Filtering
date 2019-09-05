0da39983eba7369c7ec227b6c776842c66b0dc6c
Sara HAMIDA <52857308+S4r4hFUN@users.noreply.github.com>
2019-09-05 12:30:46 +0100

    Transpose and stability fixes

---
+++ Stability/main.py
@@ -0,0 +1,50 @@
+from stability.ExperimentStability import ExperimentStability
+from stability.AutoencoderStability import AutoencoderStability
+from autoencoder.Autoencoder import Autoencoder
+
+from tools.tools import generate_landas
+
+sets_parameters = {'database_id': [0],
+                   'test_ratio': [0.1],
+                   'validation_ratio': [0.1],
+                   'learning_type': 'V'
+                   }
+
+experiment_parameters = {'mean_iterations': [1],
+                         'nb_draws': [5]
+                         }
+
+
+autoencoder_parameters = {'hidden1_units': [700],
+                          'regularisation': [0.2],
+                          'learning_rate0': [0.001],
+                          'learning_decay': [0.9],
+                          'batch_size_evaluate': [100],
+                          'batch_size_train': [35],
+                          'nb_epoch': [15],
+                          'is_test': [False],
+                          }
+
+factorisation_parameters = {'landa': [3],
+                            'iterations': [10],
+                            'dimension': [10]
+                            }
+
+stability_parameters = {'probability': [0.7, 0.8, 0.9],
+                        'subsets_number': [3],
+                        'landa_array': generate_landas(sets_number=4,
+                                                       samples_number=50),
+                        'first_learning': 'factorisation'
+                        }
+
+parameters_range = {'autoencoder': autoencoder_parameters,
+                    'factorisation': factorisation_parameters,
+                    'stability': stability_parameters,
+                    'experiments': experiment_parameters,
+                    'sets': sets_parameters
+                    }
+
+Experiment = ExperimentStability(parameters_range=parameters_range,
+                                 Autoencoder=Autoencoder,
+                                 AutoencoderStability=AutoencoderStability)
+Experiment.run()
